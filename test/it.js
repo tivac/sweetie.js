743c7c298db063d297ebcbc1fd7d7985b09304d3
Pat Cavit <pcavit@gmail.com>
2015-06-18 14:56:52 -0700

    Switch to deepEquals

--- test/it.js
+++ test/it.js
@@ -14,18 +14,32 @@ describe("Sweetie", function() {
         });
 
         it("should add a test to the current context when nested", function() {
-            var s = new Sweetie();
+            var s = new Sweetie(),
+                t = function() {};
 
             s.describe("one", function() {
-                s.it("one", function() {});
+                s.it("one", t);
 
                 s.describe("two", function() {
                     s.it("two");
                 });
             });
 
-            assert.equal(s.env.one.__tests.length, 1);
-            assert.equal(s.env.one.two.__tests.length, 1);
+            assert.deepEqual(s.env, {
+                __tests : [],
+                one : {
+                    __tests : [{
+                        fn : t,
+                        name : "one"
+                    }],
+                    two : {
+                        __tests : [{
+                            fn : undefined,
+                            name : "two"
+                        }]
+                    }
+                }
+            });
         });
     });
 });
